c998ac5e06e1e077e7256216a946435076220032
yosukesato1991 <sun88912317icloud.com>
2020-01-30 15:36:03 +0000

    first commit

---
+++ config/initializers/assets.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile+= %w(admin.css)
+Rails.application.config.assets.precompile+= %w(staff.css)
+Rails.application.config.assets.precompile+= %w(.css)
